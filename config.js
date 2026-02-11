ceb182663de24ba579a76ff8382e885358aad25e
Suhib Yaser Abed <253351588+suhaib205@users.noreply.github.com>
2026-02-11 14:26:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,20 @@
 window.STORE_CONFIG = {
   STORE_NAME: "Suhaib Store",
 
-  // ✅ لوجو (ارفع ملف logo.png بجانب index.html)
+  // ✅ اللوجو (ارفع logo.png بجانب index.html)
   LOGO_PATH: "./logo.png",
 
-  // ✅ تواصل (عبّيهم لاحقاً)
-  WHATSAPP_NUMBER: "PUT_YOUR_NUMBER_WITH_COUNTRY_CODE", // مثال: 9627XXXXXXXX
-  INSTAGRAM_URL: "PUT_YOUR_INSTAGRAM_LINK",             // مثال: https://instagram.com/username
+  // ✅ تواصل
+  WHATSAPP_NUMBER: "966574640815",
+  INSTAGRAM_URL: "https://www.instagram.com/suhaib._.store?igsh=NDFudTlyOHFhNzJ0",
 
-  // ✅ Supabase (اتركهم فاضيين الآن، ونعبيهم بعد ما نعمل Supabase)
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  // ✅ Supabase (مشروعك الجديد)
+  SUPABASE_URL: "https://waefxckwzyexguwjoyii.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_tdwHFBdAaZg7ao9nVgDqqQ_Tjiu4w6D",
 
-  // ✅ إعدادات
+  // ✅ Storage
   STORAGE_BUCKET: "product-images",
-  DEFAULT_CURRENCY: "JOD",
-};
 
+  // ✅ إعدادات عامة
+  DEFAULT_CURRENCY: "SAR",
+};
